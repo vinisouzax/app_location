29b8a9db1ee5c815d557f44967180f1ab788995d
vinicius <vinisouzax@gmail.com>
2021-08-23 21:35:59 -0300

    Mudança nas requisições

--- app/src/main/java/com/movep/movep/RetrieveHttp.java
+++ app/src/main/java/com/movep/movep/RetrieveHttp.java
@@ -31,30 +31,11 @@ public class RetrieveHttp extends AsyncTask<String, Void, JSONObject> {
             byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
             client = (HttpURLConnection) url.openConnection();
             client.setRequestMethod(strings[1]);
-            client.setRequestProperty("Content-Type",
-                    "application/json");
-            client.setRequestProperty("auth-token", strings[3]);
-            client.setDoOutput(true);
+            client.setDoOutput(false);
             client.setDoInput(true);
-            try (DataOutputStream wr = new DataOutputStream(client.getOutputStream())) {
-                wr.write(postData);
-            }
-            StringBuilder content;
-            out.println(strings[0]+ "\n" + strings[1]+ "\n" + strings[2] + "\n" + strings[3]);
-            try (BufferedReader br = new BufferedReader(
-                    new InputStreamReader(client.getInputStream()))) {
-
-                String line;
-                content = new StringBuilder();
-
-                while ((line = br.readLine()) != null) {
-                    content.append(line);
-                    content.append(System.lineSeparator());
-                }
-                client.disconnect();
-            }
-            Log.e("\n\n\nRetrive\n\n\n", "Json=  \n\n\n" + content.toString() +"\n\n\n");
-            return new JSONObject(content.toString());
+            client.connect();
+            int status = client.getResponseCode();
+            return new JSONObject("{\"status\":"+String.valueOf(status)+"}");
 
         } catch (IOException | JSONException e) {
             e.printStackTrace();
